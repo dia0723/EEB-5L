b09bd6bf814882be3554e0cfbba6c06c06b4c546
dia0723 <o3o_cadia@naver.com>
2025-06-20 09:40:59 +0900

    init: [전지연] Backend 저장소 초기화

---
+++ frontend/JiyeonJun/html-container/src/App.tsx
@@ -0,0 +1,56 @@
+import React, {lazy, Suspense, useEffect, useState} from "react";
+import ReactDOM from "react-dom/client";
+
+import { CircularProgress } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const NavigationBarApp = lazy(() => import("navigationBarApp/App"));
+const HtmlCssTestApp = lazy(() => import("htmlCssTestApp/App"));
+const JavascriptTestApp = lazy(() => import("javascriptTestApp/App"));
+const KakaoAuthenticationApp = lazy(() => import("kakaoAuthenticationApp/App"));
+const GoogleAuthenticationApp = lazy(() => import("googleAuthenticationApp/App"));
+const ReactTestApp = lazy(() => import("reactTestApp/App"));
+const RecoilBoardApp = lazy(() => import("recoilBoardApp/App"))
+const MyApp = lazy(() => import("myApp/App"))
+
+import VuetifyTailwindBoardAppWrapper from "./VuetifyTailwindBoardAppWrapper";
+
+const App = () => {
+    const [isNavigationBarLoaded, setIsNavigationBarLoaded] = useState(false);
+
+    useEffect(() => {
+        import("navigationBarApp/App")
+            .then(() => setIsNavigationBarLoaded(true))
+            .catch((err) => console.error("Failed to load navigation bar:", err));
+    }, []);
+
+    return (
+        <BrowserRouter>
+            <Suspense fallback={<CircularProgress />}>
+                <NavigationBarApp />
+
+                <Routes>
+                    <Route path="/" element={<div>Home Page</div>} />
+                    <Route path="/html-css-test" element={<HtmlCssTestApp />} />
+                    <Route path="/js-test" element={<JavascriptTestApp />} />
+                    <Route path="/kakao-authentication/*" element={<KakaoAuthenticationApp />} />
+                    <Route path="/board/*" element={<VuetifyTailwindBoardAppWrapper />} />
+                    <Route path="/react-test" element={<ReactTestApp />} />
+                    <Route path="/google-authentication/*" element={<GoogleAuthenticationApp />} />
+                    <Route path="/recoil-board/*" element={<RecoilBoardApp />} />
+                    <Route path="/my-app" element={<MyApp />} />
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
+    );
+};
+
+export default App;
+
+const container = document.getElementById("app") as HTMLElement;
+if (!container) {
+    throw new Error("Root container #app not found");
+}
+
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
---
+++ frontend/JiyeonJun/myapp/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css"
+
+const App = () => {
+    return (
+        <div className="ABC">
+           Hi!
+        </div>
+    )
+};
+
+export default App;
